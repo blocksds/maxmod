bc796100894472bc15dd5537ba90626dec6f370c
Antonio Niño Díaz <antonio_nd@outlook.com>
2021-01-24 20:50:30 +0000

    Rewrite some functions in mm_mas.s in C

---
+++ source/mm_mas_c.c
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: ISC
+//
+// Copyright (c) 2008, Mukunda Johnson (mukunda@maxmod.org)
+// Copyright (c) 2021, Antonio Niño Díaz (antonio_nd@outlook.com)
+
+#include "maxmod.h"
+
+#include "mm_mas.h"
+#include "mp_mas_structs.h"
+
+#if defined(SYS_GBA)
+#include "mm_mixer_gba.h"
+#elif defined(SYS_NDS)
+#include "mm_mixer_ds.h"
+#endif
+
+// Suspend main module and associated channels.
+void mpp_suspend(void)
+{
+    mm_active_channel *act_ch = &mm_achannels[0];
+
+#ifdef SYS_NDS
+    mm_mixer_channel *mix_ch = &mm_mix_channels[0];
+#endif
+#ifdef SYS_GBA
+    mm_mixer_channel *mix_ch = &mm_mixchannels[0];
+#endif
+
+    for (mm_word count = mm_num_ach; count != 0; count--, act_ch++, mix_ch++)
+    {
+        if ((act_ch->flags >> 6) != 0)
+            continue;
+
+#ifdef SYS_GBA
+        mix_ch->freq = 0;
+#else
+        mix_ch->freq = 0;
+        mix_ch->vol = 0;
+#endif
+    }
+}
+
+// Pause module playback.
+void mmPause(void)
+{
+    if (mmLayerMain.valid == 0)
+        return;
+
+    mmLayerMain.isplaying = 0;
+
+    mpp_suspend();
+}
+
+// Resume module playback.
+void mmResume(void)
+{
+    if (mmLayerMain.valid == 0)
+        return;
+
+    mmLayerMain.isplaying = 1;
+}
+
+// Returns true if module is playing.
+mm_bool mmActive(void)
+{
+    return mmLayerMain.isplaying;
+}
+
+// Returns true if a jingle is playing.
+mm_bool mmActiveSub(void)
+{
+    return mmLayerSub.isplaying;
+}
+
+// Set master module volume.
+//
+// volume : 0->1024
+void mmSetModuleVolume(mm_word volume)
+{
+    // Clamp volume 0->1024
+    if (volume > 1024)
+        volume = 1024;
+
+    mmLayerMain.volume = volume;
+}
+
+// Set master jingle volume.
+//
+// volume : 0->1024
+void mmSetJingleVolume(mm_word volume)
+{
+    // Clamp volume 0->1024
+    if (volume > 1024)
+        volume = 1024;
+
+    mmLayerSub.volume = volume; // mpp_layerB
+}
