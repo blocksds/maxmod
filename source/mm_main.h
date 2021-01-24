a165d60a6bf64ffda8ebe08ef70a2cc704b39a82
Antonio Niño Díaz <antonio_nd@outlook.com>
2021-01-24 01:06:40 +0000

    Rewrite mm_main_gba.s in C
    Move some code to mm_call_helpers_gba.s until it is no longer needed.

---
+++ source/mm_main.h
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: ISC
+//
+// Copyright (c) 2008, Mukunda Johnson (mukunda@maxmod.org)
+// Copyright (c) 2021, Antonio Niño Díaz (antonio_nd@outlook.com)
+
+#ifndef MM_MAIN_H
+#define MM_MAIN_H
+
+#include "mm_types.h"
+
+extern mm_word mm_ch_mask;
+
+#endif // MM_MAIN_H
