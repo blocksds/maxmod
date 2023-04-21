90bfe219455e53a1add81cd4f15af58008a3860c
Lorenzooone <lollo.lollo.rbiz@gmail.com>
2023-04-21 15:26:43 +0200

    ds: Rename file
    Also, add a missing license header.

--- source_ds7/mm_comms7.h
+++ source_ds7/mm_comms7.h
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: ISC
+//
+// Copyright (c) 2008, Mukunda Johnson (mukunda@maxmod.org)
+// Copyright (c) 2023, Lorenzooone (lollo.lollo.rbiz@gmail.com)
+
 #ifndef MM_COMMS7_H__
 #define MM_COMMS7_H__
 
