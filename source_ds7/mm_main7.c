10d4ff6ce33b673c51d171e6fdc3de3c2707394a
Lorenzooone <lollo.lollo.rbiz@gmail.com>
2023-10-22 05:29:32 +0200

    Add the rest of the smaller functions

--- source_ds7/mm_main7.c
+++ source_ds7/mm_main7.c
@@ -19,6 +19,7 @@
 #include "mm_mas.h"
 #include "mm_effect.h"
 #include "mp_defs.h"
+#include "mm_mixer.h"
 
 #if defined(SYS_GBA)
 #include "mm_main_gba.h"
@@ -127,7 +128,9 @@ static void StopActiveChannel(mm_word index)
     mix_ch->src = -1;
 #endif
 #ifdef SYS_NDS
-    mix_ch->samp_cnt = 0;
+    mix_ch->key_on = 0;
+    mix_ch->samp = 0;
+    mix_ch->tpan = 0;
     mix_ch->vol = 0;
     mix_ch->cvol = 0;
 #endif
