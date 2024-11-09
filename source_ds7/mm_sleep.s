604ac529af318f5487d2c5c2c2ae126383e386f2
GalaxyShard <galaxyshard.tech@proton.me>
2024-11-09 01:46:06 -0500

    mm_sleep.s: use UAL syntax

--- source_ds7/mm_sleep.s
+++ source_ds7/mm_sleep.s
@@ -48,9 +48,10 @@
 .equ	SoundBias,	0x08
 .equ	BiasSlideDelay, 64		// click is pretty muffled with this setting
 
-.TEXT
-.THUMB
-.ALIGN 2
+.text
+.syntax unified
+.thumb
+.align 2
 
 /****************************************************************************
  * mmEnterSleep
@@ -66,25 +67,25 @@ mmEnterSleep:
 	bl	mmSuspendIRQ_t
 	
 	ldr	r0,=UPDATE_TIMER		@ disable updates
-	mov	r1, #0				@
+	movs	r1, #0				@
 	str	r1, [r0]			@
 
 	ldr	r0,=IE				@ clear bit in IE
 	ldr	r1, [r0]			@
-	mov	r2, #0b1000			@
-	bic	r1, r2				@
+	movs	r2, #0b1000			@
+	bics	r1, r2				@
 	str	r1, [r0]			@
 	
 	bl	mmRestoreIRQ_t
 	
-	mov	r0, #0				@ reset bias level
-	mov	r1, #BiasSlideDelay		@
+	movs	r0, #0				@ reset bias level
+	movs	r1, #BiasSlideDelay		@
 	swi	SoundBias			@
 	
 	ldr	r0,=POWCNT2			@ disable speakers
 	ldrh	r1, [r0]			@
-	mov	r2, #1				@
-	bic	r1, r2				@
+	movs	r2, #1				@
+	bics	r1, r2				@
 	strh	r1, [r0]			@
 	
 	pop	{r3}
@@ -102,21 +103,21 @@ mmExitSleep:
 	
 	ldr	r0,=POWCNT2			@ enable speakers
 	ldrh	r1, [r0]			@
-	mov	r2, #1				@
-	orr	r1, r2				@
+	movs	r2, #1				@
+	orrs	r1, r2				@
 	strh	r1, [r0]			@
 	
-	mov	r0, #1				@ reset bias level
-	mov	r1, #BiasSlideDelay		@
+	movs	r0, #1				@ reset bias level
+	movs	r1, #BiasSlideDelay		@
 	swi	SoundBias			@
 
 	ldr	r0,=IE				@ set bit in IE
 	ldr	r1, [r0]			@
-	mov	r2, #0b1000			@
-	orr	r1, r2				@
+	movs	r2, #0b1000			@
+	orrs	r1, r2				@
 	str	r1, [r0]			@
 
-	ldrb	r0,=mm_mixing_mode		@ reset mixer
+	ldr	r0,=mm_mixing_mode		@ reset mixer
 	ldrb	r0, [r0]			@
 	bl	mmSelectMode			@
 	
