blocksds/maxmod

commit 5a0804b4899fccfb82c9e303df4923ce5ff21beb

core: Convert glissando effects to C

source/core/mm_mas.c
@@ -35,6 +35,8 @@
 // TODO: Make this static
 void mpp_setbpm(mpl_layer_information*, mm_word);
 void mpp_setposition(mpl_layer_information*, mm_word);
+void mppe_ChannelVolumeSlide(mm_word param, mm_module_channel *channel,
+                             mpl_layer_information *layer);
 
 static mm_word mppe_DoVibrato(mm_word period, mm_module_channel *channel,
                               mpl_layer_information *layer);
@@ -1251,7 +1253,7 @@ mm_word mpp_Process_VolumeCommand(mpl_layer_information *layer,
 
             volcmd = channel->memory[MPP_XM_VFX_MEM_GLIS];
 
-            return mppe_glis_backdoor_Wrapper(volcmd, period, act_ch, channel, layer);
+            return mppe_glis_backdoor(volcmd, period, act_ch, channel, layer);
         }
     }
     else // IT commands
@@ -1405,7 +1407,7 @@ mm_word mpp_Process_VolumeCommand(mpl_layer_information *layer,
 
                 mm_byte mem = channel->memory[MPP_GLIS_MEM];
 
-                return mppe_glis_backdoor_Wrapper(mem, period, act_ch, channel, layer);
+                return mppe_glis_backdoor(mem, period, act_ch, channel, layer);
             }
             else // Single Gxx
             {
@@ -1416,7 +1418,7 @@ mm_word mpp_Process_VolumeCommand(mpl_layer_information *layer,
 
                 mm_byte mem = channel->memory[MPP_GLIS_MEM];
 
-                return mppe_glis_backdoor_Wrapper(mem, period, act_ch, channel, layer);
+                return mppe_glis_backdoor(mem, period, act_ch, channel, layer);
             }
         }
         else if (volcmd <= 212) // Vibrato (Speed) : mppuv_vibrato
@@ -1876,6 +1878,122 @@ mm_word mppe_Portamento(mm_word param, mm_word period, mm_module_channel *channe
     return period + delta;
 }
 
+// Calculate sample address.
+static inline mm_mas_sample_info *mpp_SamplePointer(int sampleN, mpl_layer_information *layer)
+{
+    // TODO: This is also in mm_mas_arm.c as get_sample(), we need to unify both
+    // functions when the two files are reorganized.
+    return (mm_mas_sample_info *)(layer->songadr + ((mm_word *)layer->samptable)[sampleN - 1]);
+}
+
+mm_word mppe_glis_backdoor(mm_word param, mm_word period, mm_active_channel *act_ch,
+                           mm_module_channel *channel, mpl_layer_information *layer)
+{
+    if (act_ch == NULL) // Exit if no active channel
+        return period;
+
+    mm_mas_sample_info *sample = mpp_SamplePointer(act_ch->sample, layer); // Get target period
+
+    mm_word target_period = mmGetPeriod(layer, sample->frequency * 4, channel->note);
+
+    mm_word new_period;
+
+    if (layer->flags & (1 << (C_FLAGS_SS - 1))) // Test S flag
+    {
+        if (channel->period < target_period) // Slide up
+        {
+            new_period = mpph_PitchSlide_Up(channel->period, param, layer);
+
+            if (new_period > target_period)
+                new_period = target_period;
+        }
+        else if (channel->period > target_period) // Slide down
+        {
+            new_period = mpph_PitchSlide_Down(channel->period, param, layer);
+
+            if (new_period < target_period)
+                new_period = target_period;
+        }
+        else // No slide
+        {
+            return period;
+        }
+    }
+    else // Amiga
+    {
+        // TODO: Is this correct? In the original code, mpph_PitchSlide_Down and
+        // mpph_PitchSlide_Down were swapped in the Amiga side.
+
+        if (channel->period < target_period) // Slide up
+        {
+            new_period = mpph_PitchSlide_Up(channel->period, param, layer);
+
+            if (new_period > target_period)
+                new_period = target_period;
+        }
+        else if (channel->period > target_period) // Slide down
+        {
+            new_period = mpph_PitchSlide_Down(channel->period, param, layer);
+
+            if (new_period < target_period)
+                new_period = target_period;
+        }
+        else // No slide
+        {
+            return period;
+        }
+    }
+
+    mm_word old_period = channel->period;
+
+    channel->period = new_period;
+
+    int delta = new_period - old_period;
+
+    return period + delta;
+}
+
+// EFFECT Gxy: Glissando
+mm_word mppe_Glissando(mm_word param, mm_word period, mm_active_channel *act_ch,
+                       mm_module_channel *channel, mpl_layer_information *layer)
+{
+    if (layer->tick == 0)
+    {
+        if (layer->flags & (1 << (C_FLAGS_GS - 1)))
+        {
+            // Gxx is shared, IT MODE ONLY!!
+
+            if (param == 0)
+            {
+                param = channel->memory[MPP_IT_PORTAMEM];
+                channel->param = param;
+            }
+
+            channel->memory[MPP_IT_PORTAMEM] = param;
+            channel->memory[MPP_GLIS_MEM] = param; // For simplification later
+        }
+        else
+        {
+            // Gxx is separate
+            if (param == 0)
+            {
+                param = channel->memory[MPP_GLIS_MEM];
+                channel->param = param;
+            }
+
+            channel->memory[MPP_GLIS_MEM] = param;
+
+            return period;
+        }
+    }
+
+    param = channel->memory[MPP_GLIS_MEM];
+
+    period = mppe_glis_backdoor(param, period, act_ch, channel, layer);
+
+    return period;
+}
+
 // EFFECT Hxy: Vibrato
 mm_word mppe_Vibrato(mm_word param, mm_word period, mm_module_channel *channel,
                      mpl_layer_information *layer)
@@ -1956,6 +2074,19 @@ mm_word mppe_VibratoVolume(mm_word param, mm_word period, mm_module_channel *cha
     mppe_VolumeSlide(param, channel, layer);
 
     return new_period;
+}
+
+// EFFECT Lxy: Portamento+Volume Slide
+mm_word mppe_PortaVolume(mm_word param, mm_word period, mm_active_channel *act_ch,
+                         mm_module_channel *channel, mpl_layer_information *layer)
+{
+    mm_word mem = channel->memory[MPP_GLIS_MEM];
+
+    period = mppe_Glissando(mem, period, act_ch, channel, layer);
+
+    mppe_ChannelVolumeSlide(param, channel, layer);
+
+    return period;
 }
 
 // EFFECT Mxy: Set Channel Volume

source/core/mm_mas.s
@@ -1008,57 +1008,6 @@ mpph_ProcessEnvelope:			@ params={count,node,address}
 	bx	lr
 .pool
 
-@=============================================================================
-@                                EFFECT MEMORY
-@=============================================================================
-
-.text
-
-.equ	MPP_GLIS_MEM,		0
-.equ	MPP_IT_PORTAMEM,	2
-
-/******************************************************************************
- *
- * XM Volume Commands
- *
- ******************************************************************************/
-
-.align 2
-					.global mppe_glis_backdoor_Wrapper
-					.thumb_func
-mppe_glis_backdoor_Wrapper:
-
-	push	{r5-r7, lr}
-	mov	r7, r8
-	push	{r7}
-
-	// r1 = volcmd
-	// r5 = period
-	// r6 = act_ch
-	// r7 = channel
-	// r8 = layer
-
-	movs	r1, r0
-	movs	r5, r1
-	movs	r6, r2
-	movs	r7, r3
-	// This argument is passed on the stack by the caller, but 6 registers have
-	// been pushed to the stack as well.
-	ldr	r0, [sp, #6 * 4]
-	mov	r8, r0
-
-	push {r1, lr}
-
-	bl	.mppe_glis_backdoor
-
-	movs	r0, r5
-
-	pop	{r7}
-	mov	r8, r7
-	pop	{r4-r7}
-	pop	{r1}
-	bx	r1
-
 /******************************************************************************
  *
  * Module Effects
@@ -1124,12 +1073,12 @@ mpp_Process_Effect:
 	b	mppe_VolumeSlide_Wrapper
 	b	mppe_Portamento_Wrapper
 	b	mppe_Portamento_Wrapper
-	b	mppe_Glissando
+	b	mppe_Glissando_Wrapper
 	b	mppe_Vibrato_Wrapper
 	b	mppe_todo // Tremor
 	b	mppe_Arpeggio_Wrapper
 	b	mppe_VibratoVolume_Wrapper
-	b	mppe_PortaVolume
+	b	mppe_PortaVolume_Wrapper
 	b	mppe_ChannelVolume_Wrapper
 	b	mppe_ChannelVolumeSlide_Wrapper
 	b	mppe_SampleOffset_Wrapper
@@ -1229,137 +1178,25 @@ mppe_Portamento_Wrapper: @ EFFECT Exy/Fxy: Portamento
 .align 2
 .thumb_func
 @---------------------------------------------------------------------------------
-mppe_Glissando:					@ EFFECT Gxy: Glissando
+mppe_Glissando_Wrapper: @ EFFECT Gxy: Glissando
 @---------------------------------------------------------------------------------
+	push	{lr}
 
-	mov	r2, r8
-	ldrb	r2, [r2, #MPL_TICK]	@ r2 = tick#
-	cmp	r2, #0			@ Z flag = tick0
-	bne	.mppe_glis_ot
-	
+	// This argument is passed on the stack by the caller
 	mov	r0, r8
-	ldrb	r0, [r0, #MPL_FLAGS]
-	lsrs	r0, #C_FLAGS_GS
-	bcc	2f
-	@ gxx is shared, IT MODE ONLY!!
-	cmp	r1, #0
-	bne	3f
-	ldrb	r1, [r7, #MCH_MEMORY+MPP_IT_PORTAMEM]
-	strb	r1, [r7, #MCH_PARAM]
-3:	strb	r1, [r7, #MCH_MEMORY+MPP_IT_PORTAMEM]
-	strb	r1, [r7, #MCH_MEMORY+MPP_GLIS_MEM]	@ for simplification later
-	b	.mppe_glis_ot
-	
-2:	@ gxx is separate
-	cmp	r1, #0
-	bne	3f
-	
-	ldrb	r1, [r7, #MCH_MEMORY+MPP_GLIS_MEM]
-	strb	r1, [r7, #MCH_PARAM]
-3:	strb	r1, [r7, #MCH_MEMORY+MPP_GLIS_MEM]
+	push	{r0}
 
-	bx	lr
-//	b	.mppe_glis_exit
-.mppe_glis_ot:
-	
-	push	{lr}					// save return address
-
-	ldrb	r1, [r7, #MCH_MEMORY+MPP_GLIS_MEM]
-
-	push	{r1}
-
-.mppe_glis_backdoor:
-	
-	cmp	r6, #0					// exit if no active channel
-	bne	1f					//
-	pop	{r1,r3}					//
-	bx	r3					//
-1:							//
-	
-	ldrb	r0, [r6, #MCA_SAMPLE]			// get target period
-	subs	r0, #1					//
-	mpp_SamplePointer				//
-	ldrh	r1, [r0, #C_MASS_FREQ]			//
-	lsls	R1, #2					//
-	ldrb	r2, [r7, #MCH_NOTE]			//
-	mov	r0,r8
-	ldr	r3,=mmGetPeriod				//
-	bl	mpp_call_r3				//
-	
-	pop	{r1}					// r1 = parameter
-	push	{r0}					// 
-	
-	movs	r3, r0					// r3 = target period
-	movs	r2, #MCH_PERIOD				// r0 = current period
-	ldr	r0, [r7, r2]				//
-	mov	r2, r8					// test S flag
-	ldrb	r2, [r2, #MPL_FLAGS]			//
-	lsrs	r2, #C_FLAGS_SS				//
-	bCC	.mppe_glis_amiga
-	cmp	r0, r3
-	blt	.mppe_glis_slideup
-	bgt	.mppe_glis_slidedown
-.mppe_glis_noslide:
-	pop	{r0}
-	pop	{r3}
-	bx	r3
-
-.mppe_glis_slideup:
-	mov	r2, r8
-	bl	mpph_PitchSlide_Up
-	pop	{r1}
-	
-	cmp	r0, r1
-	blt	.mppe_glis_store
 	movs	r0, r1
-	b	.mppe_glis_store
-.mppe_glis_slidedown:
-	mov	r2, r8
-	bl	mpph_PitchSlide_Down
-	pop	{r1}
-	cmp	r0, r1
-	bgt	.mppe_glis_store
-	movs	r0, r1
-.mppe_glis_store:
-	
-	movs	r2, #MCH_PERIOD
-	ldr	r1, [r7, r2] @#MCA_PERIOD]
-	str	r0, [r7, r2] @#MCA_PERIOD]
-	subs	r0, r1
-	adds	r5, r0
-	
-.mppe_glis_exit:
-	pop	{r3}
-	bx	r3
+	movs	r1, r5
+	movs	r2, r6
+	movs	r3, r7
 
+	bl	mppe_Glissando
+	movs	r5, r0
 
-	//bx	lr
-
-.mppe_glis_amiga:
-
-	cmp	r0, r3
-	blt	.mppe_glis_amiga_up
-	bgt	.mppe_glis_amiga_down
-	pop	{r0}
-	pop	{r3}
-	bx	r3
-
-.mppe_glis_amiga_down:
-	mov	r2, r8
-	bl	mpph_PitchSlide_Up
-	pop	{r1}
-	cmp	r0, r1
-	bgt	.mppe_glis_store
-	movs	r0, r1
-	b	.mppe_glis_store
-.mppe_glis_amiga_up:
-	mov	r2, r8
-	bl	mpph_PitchSlide_Down
-	pop	{r1}
-	cmp	r0, r1
-	blt	.mppe_glis_store
-	movs	r0, r1
-	b	.mppe_glis_store
+	pop		{r2}
+	pop		{r2}
+	bx	r2
 
 .align 2
 .thumb_func
@@ -1426,24 +1263,25 @@ mppe_VibratoVolume_Wrapper: @ EFFECT Kxy: Vibrato+Volume Slide
 .align 2
 .thumb_func
 @---------------------------------------------------------------------------------
-mppe_PortaVolume:			@ EFFECT Lxy: Portamento+Volume Slide
+mppe_PortaVolume_Wrapper:			@ EFFECT Lxy: Portamento+Volume Slide
 @---------------------------------------------------------------------------------
-
 	push	{lr}
 
-	mov	r2, r8
-	ldrb	r2, [r2, #MPL_TICK]	@ r2 = tick#
+	// This argument is passed on the stack by the caller
+	mov	r0, r8
+	push	{r0}
 
-	push	{r1,r2}
-	ldrb	r1, [r7, #MCH_MEMORY+MPP_GLIS_MEM]
-	bl	mppe_Glissando
-	pop	{r1, r2}
+	movs	r0, r1
+	movs	r1, r5
+	movs	r2, r6
+	movs	r3, r7
 
-	bl	mppe_VolumeSlide_Wrapper
+	bl	mppe_PortaVolume
+	movs	r5, r0
 
-	pop	{r0}
-	bx	r0
-//	pop	{pc}
+	pop		{r2}
+	pop		{r2}
+	bx	r2
 
 .align 2
 .thumb_func

source/core/mm_mas_arm.h
@@ -57,7 +57,7 @@ mm_word mpph_PitchSlide_Down(mm_word, mm_word, mpl_layer_information*);
 mm_word mpph_LinearPitchSlide_Down(mm_word period, mm_word slide_value, mpl_layer_information *layer);
 mm_word mpph_FinePitchSlide_Down(mm_word period, mm_word slide_value, mpl_layer_information *layer);
 
-mm_word mppe_glis_backdoor_Wrapper(mm_word volcmd, mm_word period, mm_active_channel *act_ch,
-                                   mm_module_channel *channel, mpl_layer_information *layer);
+mm_word mppe_glis_backdoor(mm_word param, mm_word period, mm_active_channel *act_ch,
+                           mm_module_channel *channel, mpl_layer_information *layer);
 
 #endif // MM_MAS_ARM_H

commit db66dca4a65e11793018af2e90e287a2553f531c

core: Fix Portamento + VolumeSlide effect
Commit 5a0804b4899f ("core: Convert glissando effects to C") introduced
a call to the wrong function. Assembly code was calling the correct
function, mppe_VolumeSlide(), but the disassembled code switched it to a
call to mppe_ChannelVolumeSlide().

source/core/mm_mas.c
@@ -2152,7 +2152,7 @@ mm_word mppe_PortaVolume(mm_word param, mm_word period, mm_active_channel *act_c
 
     period = mppe_Glissando(mem, period, act_ch, channel, layer);
 
-    mppe_ChannelVolumeSlide(param, channel, layer);
+    mppe_VolumeSlide(param, channel, layer);
 
     return period;
 }