f56841d33d1cad845d0cb4552b7b7bb51f2a8ae0
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-07-23 00:37:33 +0100

    chore: Small cleanup of code in mm_mas.s
    Remove trailing whitespace, commented-out code, etc.

--- source/core/mm_mas.s
+++ source/core/mm_mas.s
@@ -6,24 +6,16 @@
 #include "mp_mas_structs.inc"
 #include "mp_defs.inc"
 
-//-----------------------------------------------------------------------------
 #ifdef SYS_GBA
-//-----------------------------------------------------------------------------
-
 #include "mp_mixer_gba.inc"
 #include "swi_gba.inc"
-
-//-----------------------------------------------------------------------------
 #endif
-//-----------------------------------------------------------------------------
 
-//-----------------------------------------------------------------------------
 #ifdef SYS_NDS
-//-----------------------------------------------------------------------------
 #include "mp_mixer_ds.inc"
 #include "swi_nds.inc"
-//-----------------------------------------------------------------------------
 #endif
+
 //-----------------------------------------------------------------------------
 
     .syntax unified
@@ -34,7 +26,7 @@
 
 
 /******************************************************************************
- * 
+ *
  * Macros
  *
  ******************************************************************************/
@@ -49,10 +41,10 @@
  ******************************************************************************/
 .macro mpp_InstrumentPointer
 	mov	r1, r8
-	ldr	r2,[r1,#MPL_SONGADR]	
-	ldr	r1,[r1,#MPL_INSTTABLE]	
+	ldr	r2,[r1,#MPL_SONGADR]
+	ldr	r1,[r1,#MPL_INSTTABLE]
 	lsls	r0, #2
-	ldr	r0,[r1,r0]		
+	ldr	r0,[r1,r0]
 	adds	r0, r2
 .endm
 
@@ -64,16 +56,14 @@
  * Returns in r0
  * Trashes r1, r2
  ******************************************************************************/
-.macro mpp_SamplePointer	
+.macro mpp_SamplePointer
 	mov	r1, r8
-	ldr	r2,[r1,#MPL_SONGADR]	
-	ldr	r1,[r1,#MPL_SAMPTABLE]	
+	ldr	r2,[r1,#MPL_SONGADR]
+	ldr	r1,[r1,#MPL_SAMPTABLE]
 	lsls	r0, #2
-	ldr	r0,[r1,r0]		
+	ldr	r0,[r1,r0]
 	adds	r0, r2
 .endm
-
-
 
 /******************************************************************************
  *
@@ -137,7 +127,7 @@ mpp_Update_ACHN_notest:
 @------------------------------------------------------------------------
 @ Envelope Processing
 @------------------------------------------------------------------------
-	
+
 	ldrb	r0, [r6, #MCA_INST]
 	subs	r0, #1
 	bcs	1f
@@ -179,26 +169,26 @@ mpp_Update_ACHN_notest:
 .mppt_achn_nostart:
 
 	@ set pitch
-	
+
 	ldrb	r0, [r6, #MCA_SAMPLE]		@ get sample#
 	subs	r0, #1				@ ..
 	bcc	.mppt_achn_setvolumeA
 	mpp_SamplePointer		@ quit if invalid
 	push	{r0}
-	
+
 	mov	r1, r8
 	ldrb	r1, [r1, #MPL_FLAGS]
 	lsrs	r1, #C_FLAGS_SS
 	bcc	.mppt_achn_amigafreqs
 .mppt_achn_linearfreqs:
-	
+
 	ldrh	r0, [r0, #C_MASS_FREQ]		@ get C5SPEED
 	lsls	R0, #2
 	lsrs	r1, r5, #8			@ do some stuff...
 	muls	r1, r0				@ ... period * freq?
-	
+
 	lsrs	r1, #8
-	
+
 	ldr	r0,=mpp_clayer
 	ldrb	r0, [r0]
 	cmp	r0, #0
@@ -209,32 +199,27 @@ mpp_Update_ACHN_notest:
 	lsrs	r1, #10
 1:
 
-	#ifdef SYS_GBA
-
+#ifdef SYS_GBA
 	ldr	r0,=(4096*65536)/15768
 	muls	r0, r1
 	lsrs	r0, #16
 	str	r0, [r4, #MIXER_CHN_FREQ]
-	
-	#else
-
+#else
 	ldr	r0,=MIXER_SCALE
 	muls	r0, r1
 	lsrs	r0, #16+1
 	strh	r0, [r4, #MIXER_CHN_FREQ]
-	
-	//strh	r1, [r4, #MIXER_CHN_FREQ]
-	#endif
-	
+#endif
+
 	b	.mppt_achn_setvolume
-	
+
 .mppt_achn_amigafreqs:
 	ldr	r0,=MOD_FREQ_DIVIDER_PAL
 	movs	r1, r5
-	
+
 	beq	.mppt_achn_setvolume		@ 0 is a bad period
 	swi	SWI_DIVIDE
-	
+
 	ldr	r1,=mpp_clayer
 	ldrb	r1, [r1]
 	cmp	r1, #0
@@ -245,72 +230,68 @@ mpp_Update_ACHN_notest:
 	lsrs	r0, #10
 1:
 
-	#ifdef SYS_GBA
-
+#ifdef SYS_GBA
 	ldr	r1,=(4096*65536)/15768
 	muls	r0, r1
 	lsrs	r0, #16
-
 	str	r0, [r4, #MIXER_CHN_FREQ]
-	#else
-
+#else
 	ldr	r1,=MIXER_SCALE		@ calculate ds mixer timing
 	muls	r0, r1
 	lsrs	r0, #16+1
 	strh	r0, [r4, #MIXER_CHN_FREQ]
-	#endif
+#endif
 
 @----------------------------------------------------------------------------------------------------
 .mppt_achn_setvolume:
 @----------------------------------------------------------------------------------------------------
-	
+
 	@ set volume
-	
+
 	pop	{r0}
 						@ <-- stepped oct 28, 3:27pm
 	ldrb	r3, [r0, #C_MASS_GV]		@ SV, 6-bit
 	ldrb	r0, [r6, #MCA_INST]
 	subs	r0, #1
 	bcs	1f
-	
-.thumb_func
-.mppt_achn_setvolumeA:	
+
+.mppt_achn_setvolumeA:
 	movs	r1, #0
 	b	.mppt_achn_badinstrument
-1:	
+1:
 	mpp_InstrumentPointer
 	ldrb	r0, [r0, #C_MASI_GVOL]	@ IV, 7-bit
 	muls	r3, r0
-	
+
 	ldr	r1,=mpp_vars
 	ldrb	r0, [r1, #MPV_AFVOL]	@ ((CV*VOL)/32*VEV/64) 7-bit
-	
+
 	muls	r3, r0
-	
+
 	mov	r1, r8			@ get global vollume
 	ldrb	r0, [r1, #MPL_FLAGS]
 	lsrs	r0, #4
 	ldrb	r0, [r1, #MPL_GV]	@ ..		     7-bit
-	
+
 	bcc	1f
 	lsls	r0, #1			@ xm mode global volume is only 0->64, shift to 0->128
-	
+
 1:	muls	r3, r0			@ multiply
-	
+
 	lsrs	r3, #10
-	
+
 	ldrh	r0, [r6, #MCA_FADE]
-	
-	
+
+
 	muls	r3, r0
-	
+
 	lsrs	r3, r3, #10
-	
+
 	mov	r0, r8
 	movs	r1, #MPL_VOLUME
 	ldrh	r0, [r0, r1]
 	muls	r3, r0
-	
+
 //------------------------------------------------
 #ifdef SYS_NDS
 	lsrs	r1, r3, #19-3-5 ///#19-3	(new 16-bit levels!)
@@ -323,9 +304,7 @@ mpp_Update_ACHN_notest:
 //	lsrs	r3, r1, #3		(new 16-bit levels!)
 	lsrs	r3, r1, #8
 	strb	r3, [r6, #MCA_FVOL]
-
 #else
-
 	lsrs	r1, r3, #19
 	cmp	r1, #255			@ clip values over 255
 	blt	1f
@@ -333,14 +312,12 @@ mpp_Update_ACHN_notest:
 1:
 .mppt_achn_badinstrument:
 	strb	r1, [r6, #MCA_FVOL]
-
 #endif
 
 	cmp	r1, #0
 	bne	.mppt_achn_audible
-	
-	#ifdef SYS_NDS			// nds has volume ramping!
-	
+
+#ifdef SYS_NDS			// nds has volume ramping!
 	ldrb	r3, [r6, #MCA_TYPE]
 	cmp	r3, #ACHN_BACKGROUND
 	bne	1f
@@ -350,8 +327,8 @@ mpp_Update_ACHN_notest:
 	ldrh	r3, [r4, #MIXER_CHN_CVOL]
 	cmp	r3, #0
 	beq	.mppt_achn_not_audible
-	#endif
-	
+#endif
+
 1:	ldrb	r3, [r6, #MCA_FLAGS]
 	movs	r2, #MCAF_ENVEND
 	tst	r3, r2
@@ -359,12 +336,6 @@ mpp_Update_ACHN_notest:
 	movs	r2, #MCAF_KEYON
 	tst	r3, r2
 	bne	.mppt_achn_audible
-	
-//	#ifdef SYS_NDS
-//	ldrh	r3, [r4, #MIXER_CHN_CVOL]	// nds has volume ramping!!
-//	cmp	r3, #0
-//	bne	.mppt_achn_audible
-//	#endif
 
 .mppt_achn_not_audible:
 	@ STOP CHANNEL
@@ -389,34 +360,26 @@ mpp_Update_ACHN_notest:
 	movs	r1, #255
 	strb	r1, [r0, #MCH_ALLOC]
 .mpp_achn_noparent:
-	
+
 	movs	r1, #ACHN_DISABLED
 	strb	r1, [r6, #MCA_TYPE]
 	b	.mpp_achn_updated
 .mppt_achn_audible:
-	
-#ifdef SYS_NDS
-	
-	strh	r1, [r4, #MIXER_CHN_VOL]
-	
-#else
-	
-	strb	r1, [r4, #MIXER_CHN_VOL]
-	
-#endif
-	
-#ifdef SYS_GBA						// check if mixer channel has ended
 
+#ifdef SYS_NDS
+	strh	r1, [r4, #MIXER_CHN_VOL]
+#else
+	strb	r1, [r4, #MIXER_CHN_VOL]
+#endif
+
+#ifdef SYS_GBA						// check if mixer channel has ended
 	ldr	r0, [r4, #MIXER_CHN_SRC]
 	asrs	r0, #31
 	beq	1f
-
 #else
-
 	ldr	r0, [r4, #MIXER_CHN_SAMP]
 	lsls	r0, #8
 	bne	1f
-	
 #endif
 
 	ldrb	r3, [r6, #MCA_TYPE]
@@ -450,7 +413,7 @@ mpp_Update_ACHN_notest:
 	movs	r3, #MPV_PANPLUS
 	ldrsh	r0, [r1,r3]
 	ldrb	r1, [r6, #MCA_PANNING]
-	
+
 	adds	r1, r0
 
 	cmp	r1, #0
@@ -462,20 +425,19 @@ mpp_Update_ACHN_notest:
 	movs	r1, #255
 .mpp_achn_clippan2:
 
-	#ifdef SYS_NDS
+#ifdef SYS_NDS
 	lsrs	r1, #1
 	ldrb	r0, [r4, #MIXER_CHN_CNT]
 	lsrs	r0, #7
 	lsls	r0, #7
 	orrs	r0, r1
 	strb	r0, [r4, #MIXER_CHN_CNT]
-	#endif
-	
-	#ifdef SYS_GBA
+#endif
+
+#ifdef SYS_GBA
 	strb	r1, [r4, #MIXER_CHN_PAN]
-	#endif
-	
-	
+#endif
+
 .mpp_achn_updated:
 	pop	{r4}
 	pop	{r0}
