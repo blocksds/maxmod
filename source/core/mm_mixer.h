bf87d9508f1ec60bd8f63d07de11020b2dbb65de
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-07-25 22:42:28 +0100

    core: Add note about two unused functions

--- source/core/mm_mixer.h
+++ source/core/mm_mixer.h
@@ -10,14 +10,14 @@
 
 extern mm_word mm_mixlen;
 
-void mmMixerSetSource(int channel, mm_word p_sample);
 void mmMixerSetVolume(int channel, mm_word volume);
 void mmMixerSetPan(int channel, mm_byte panning);
 void mmMixerSetFreq(int channel, mm_word rate);
 void mmMixerMulFreq(int channel, mm_word factor);
 void mmMixerStopChannel(int channel);
+
+// TODO: The functions below aren't used anywhere, but they are implemented for GBA and DS.
+void mmMixerSetSource(int channel, mm_word p_sample);
 mm_word mmMixerChannelActive(int channel);
 
 #endif // MM_MIXER_H
-
-
