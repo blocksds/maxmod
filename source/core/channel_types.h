7c296039c5e5d4442a3c969bdeaf7adad680586d
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-08-22 20:48:01 +0100

    gba: Fix comment

--- source/core/channel_types.h
+++ source/core/channel_types.h
@@ -183,7 +183,7 @@ static_assert(sizeof(mm_mixer_channel) == 16);
 // A GBA mixer channel is active if "src & (1 << 31)" is zero.
 typedef struct {
     uintptr_t   src;
-    mm_word     read; // Fixed point 22.12. See MP_SAMPFRAC
+    mm_word     read; // Fixed point 20.12. See MP_SAMPFRAC
     mm_byte     vol;
     mm_byte     pan;
     mm_byte     unused_0;
